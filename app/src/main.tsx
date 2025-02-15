0a4280f07309eb809e614b442a220088fd4eb95a
EskildVeren <eskild.overen@gmail.com>
2025-02-15 20:47:13 +0100

    feat: :sparkles: add temporary login page

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/login/",
+    element: <Login />,
   },
 ]);
 
